afc2c1e27b44deca8731317417c259f66e958311
INDIAjuliano <juliano.gasii1@gmail.com>
2024-01-05 21:21:38 +0300

    produit

--- ecommerc/app/views.py
+++ ecommerc/app/views.py
@@ -1,5 +1,16 @@
+from django.db.models import Count
+from urllib import request
 from django.shortcuts import render
+from django.shortcuts import render
+from django.views import View
+from . models import Product
 
 # Create your views here.
 def home(request):
     return render(request, "app/home.html")
+
+class CategoryView(View):
+    def get(self,request,val):
+        product = Product.objects.filter(category=val)
+        title = Product.objects.filter(category=val).values('title')
+        return render(request, "app/category.html",locals())
